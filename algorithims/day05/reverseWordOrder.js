ef7bb0e11d9d16ef346aac727250ad11e4410c9e
tmax818 <tylermaxwell661@gmail.com>
2023-04-10 07:26:50 -0700

    update algo

---
+++ algorithims/day05/reverseWordOrder.js
@@ -0,0 +1,25 @@
+/* 
+  Reverse Word Order
+  Given a string of words (with spaces)
+  return a new string with words in reverse sequence.
+*/
+
+const str1 = "This is a test";
+const expected1 = "test a is This";
+
+const str2 = "hello";
+const expected2 = "hello";
+
+const str3 = "   This  is a   test  ";
+const expected3 = "test a is This";
+
+/**
+ * Reverses the order of the words but not the words themselves form the given
+ * string of space separated words.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} wordsStr A string containing space separated words.
+ * @returns {string} The given string with the word order reversed but the words
+ *    themselves are not reversed.
+ */
+function reverseWordOrder(wordsStr) {}

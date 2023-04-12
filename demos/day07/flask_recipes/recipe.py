c17b62508281a3254c0e3e11a329107182042875
tmax818 <tylermaxwell661@gmail.com>
2023-04-12 14:16:07 -0700

    update

--- demos/day07/flask_recipes/recipe.py
+++ demos/day07/flask_recipes/recipe.py
@@ -2,43 +2,43 @@
 from mysqlconnection import connectToMySQL
 from pprint import pprint
 
-DATABASE = "books_schema"
+DATABASE = "recipes_schema"
 
-class Book:
+class Recipe:
     
     def __init__(self, data:dict) -> None:
         self.id = data['id']
-        self.title = data['title']
-        self.author = data['author']
+        self.name = data['name']
+        self.chef = data['chef']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         
-    def show_title(self):
-        return self.title
+    def show_name(self):
+        return self.name
     
     #! CREATE
     @classmethod
     def save(cls, data):
-        query = "INSERT INTO books (title, author) VALUES (%(title)s, %(author)s);"
+        query = "INSERT INTO recipes (name, chef) VALUES (%(name)s, %(chef)s);"
         return connectToMySQL(DATABASE).query_db(query, data)
        
     #! READ ALL 
     @classmethod
     def get_all(cls):
-        query = "SELECT * FROM books;"
+        query = "SELECT * FROM recipes;"
         results = connectToMySQL(DATABASE).query_db(query)
         pprint(results)
-        books = []
-        for book_dict in results:
-            books.append(cls(book_dict))
-        return books
+        recipes = []
+        for recipe_dict in results:
+            recipes.append(cls(recipe_dict))
+        return recipes
     
     #! READ ONE
     @classmethod
-    def get_book(cls, data):
-        query = "SELECT * FROM books WHERE id = %(id)s;"
+    def get_recipe(cls, data):
+        query = "SELECT * FROM recipes WHERE id = %(id)s;"
         result = connectToMySQL(DATABASE).query_db(query, data)
         print(result[0])
-        book = Book(result[0])
-        return book
+        recipe = Recipe(result[0])
+        return recipe
         
--- demos/day07/flask_recipes/server.py
+++ demos/day07/flask_recipes/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request  # Import Flask to allow us to create our app
-from book import Book
+from recipe import Recipe
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 
@@ -11,23 +11,23 @@ def new():
 @app.route('/create', methods=['post'])
 def create():
     print(request.form)
-    Book.save(request.form)
+    Recipe.save(request.form)
     return redirect('/')
 
 #! READ ALL
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def index():
-    books = Book.get_all()
-    print(books)
-    return render_template('index.html', books = books)  # Return the string 'Hello World!' as a response
+    recipes = Recipe.get_all()
+    print(recipes)
+    return render_template('index.html', recipes = recipes)  # Return the string 'Hello World!' as a response
 
 #! READ ONE
-@app.route('/book/<int:id>')
+@app.route('/recipe/<int:id>')
 def show(id):
     data = {'id': id}
-    book = Book.get_book(data)
-    print(book)
-    return render_template('show.html', book = book)
+    recipe = Recipe.get_recipe(data)
+    print(recipe)
+    return render_template('show.html', recipe = recipe)
 
 #! UPDATE
 

13ae751f4ce4477f2e42b3911618017d46cdd718
tmax818 <tylermaxwell661@gmail.com>
2023-04-21 10:37:21 -0700

    update

--- demos/day15/flask_ram/server.py
+++ demos/day15/flask_ram/server.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, jsonify
 import requests
 from pprint import pprint
 app = Flask(__name__)
+APIkey = "61107cb5aeddc3fe473d540e1768ff09"
+lat = 35.221996
+lon = -101.831299
 
 @app.route('/')
 def index():
@@ -14,6 +17,24 @@ def character():
     characters = requests.get("https://rickandmortyapi.com/api/character").json()
     pprint(characters)
     return render_template('index.html', characters = characters['results'])
+
+@app.route('/search', methods = ['post'])
+def search():
+    print(request.form)
+    res = requests.get(f"https://rickandmortyapi.com/api/character/?name={request.form['name']}").json()
+    print(res)
+    return render_template('index.html', res = res['results'])
+
+@app.route('/weather')
+def weather():
+    data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIkey}").json()
+    print(data)
+    return redirect('/')
+
+@app.route('/api/info')
+def info():
+    person = {'name': 'zach', 'age': 23}
+    return jsonify(person)
     
 
 
